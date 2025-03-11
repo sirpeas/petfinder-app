d8dc7622011a5d601f2645a748185fa070508d91
sirpeas <4818642+sirpeas@users.noreply.github.com>
2025-03-11 15:14:41 +0700

    [FEATURE] Create Heading component

---
+++ src/components/atoms/Heading/Heading.tsx
@@ -0,0 +1,30 @@
+import { ElementType, FC } from 'react';
+
+import { Props } from './types';
+import clsx from 'clsx';
+
+const LEVEL_TO_CLASSNAMES = {
+  h1: "text-5xl font-bold",
+  h2: "text-3xl font-semibold",
+  h3: "text-2xl font-medium",
+  h4: "text-xl font-medium",
+  h5: "text-lg font-medium",
+  h6: "text-base font-medium",
+};
+
+
+export const Heading: FC<Props> = (props) => {
+  const { as, className, children } = props;
+  if (!as) {
+    return null;
+  }
+
+  const Heading = as as ElementType;
+
+  return (
+    <Heading className={clsx(LEVEL_TO_CLASSNAMES[as], className)}>
+      {children}
+    </Heading>
+  );
+};
+Heading.displayName = 'Heading';
