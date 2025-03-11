39e2ad2fc8912867bdc474e9bc9f6fca6d9d5b25
sirpeas <4818642+sirpeas@users.noreply.github.com>
2025-03-11 15:01:56 +0700

    [FEATURE] Add all nav items with empty pages

--- src/constants/NAVIGATION.tsx
+++ src/constants/NAVIGATION.tsx
@@ -1,7 +1,15 @@
-import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr";
+import { MagnifyingGlass, Shuffle, House } from "@phosphor-icons/react/dist/ssr";
 import { ROUTES } from './ROUTES';
 
 export const NAVIGATION = [{
+  url: ROUTES.HOME,
+  label: 'Home',
+  name: <House size={24} />,
+}, {
+  url: ROUTES.SHUFFLE_SEARCH,
+  label: 'Shuffle Search',
+  name: <Shuffle size={24} />,
+}, {
   url: ROUTES.SEARCH,
   label: 'Search',
   name: <MagnifyingGlass size={24} />,
--- src/constants/ROUTES.ts
+++ src/constants/ROUTES.ts
@@ -1,4 +1,5 @@
 export const ROUTES = {
   HOME: '/',
+  SHUFFLE_SEARCH: '/shuffle-search',
   SEARCH: '/search',
 } as const;
